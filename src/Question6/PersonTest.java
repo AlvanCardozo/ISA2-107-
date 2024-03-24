ee51c1d4bfe051ce7dee36365bb71abfd4d18ea7
Alvan Cardozo <cardozoalvan@gmail.com>
2024-03-24 23:48:16 +0530

    sixth commit

---
+++ src/Question6/PersonTest.java
@@ -0,0 +1,37 @@
+package Question6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PersonTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+    public void testSetNameAndGetAge() {
+        Person person = new Person("John", 30);
+        person.setName("Alice");
+        assertEquals("Alice", person.getName());
+        assertEquals(30, person.getAge());
+    }
+
+}
