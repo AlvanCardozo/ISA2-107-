cd82150b036607222b054c05d4ab170526c2943c
Alvan Cardozo <cardozoalvan@gmail.com>
2024-03-24 23:35:09 +0530

    forth commit

---
+++ src/Question4/countdigitTest.java
@@ -0,0 +1,39 @@
+package Question4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class countdigitTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+    public void testCountDigitsPositive() {
+        assertEquals(4, countDigits.countDigits(1234));
+    }
+
+    @Test
+    public void testCountDigitsZero() {
+        assertEquals(1, countDigits.countDigits(0));
+    }
+
+}
