706f4c6af9f53cb5f764f713b270a2bb4653af35
Alvan Cardozo <cardozoalvan@gmail.com>
2024-03-24 23:59:31 +0530

    eighth commit

---
+++ src/Question8/EmployeeTest.java
@@ -0,0 +1,37 @@
+package Question8;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+    public void testCalculateDA() {
+        Employee employee = new Employee("John Doe", "Software Engineer", 50000);
+        double da=employee.calculateDA();
+        assertEquals(5000.0,da,0.01);
+    }
+
+}
+
